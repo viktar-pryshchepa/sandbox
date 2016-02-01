3a754babf4d922f51d9bf3c1b389b8600e62822a
viktar.pryshchepa <july16@tut.by>
2016-02-01 23:00:54 +0300

    'bla'

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
 var elixir = require('laravel-elixir');
+var gulp = require('gulp');
 
 /*
  |--------------------------------------------------------------------------
@@ -11,6 +12,32 @@ var elixir = require('laravel-elixir');
  |
  */
 
+
+
+gulp.task("copyfiles", function() {
+
+  gulp.src("vendor/bower_dl/jquery/dist/jquery.js")
+      .pipe(gulp.dest("resources/assets/js/"));
+
+  gulp.src("vendor/bower_dl/bootstrap/dist/css/bootstrap.css")
+      .pipe(gulp.dest("resources/assets/css/bootstrap"));
+
+  gulp.src("vendor/bower_dl/bootstrap/dist/js/bootstrap.js")
+      .pipe(gulp.dest("resources/assets/js/"));
+
+  gulp.src("vendor/bower_dl/bootstrap/dist/fonts/**")
+      .pipe(gulp.dest("public/assets/fonts"));
+
+});
+
 elixir(function(mix) {
-    mix.sass('app.scss');
+  mix.scripts([
+        'js/jquery.js',
+        'js/bootstrap.js'
+      ],
+      'public/assets/js/admin.js',
+      'resources/assets'
+  );
+
+  mix.sass('app.scss');
 });
